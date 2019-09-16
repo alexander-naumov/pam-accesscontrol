e80d6f49307c10a9be22fc151332fc245d67e361
Alexander Naumov <alexander_naumov@opensuse.org>
2019-09-16 16:45:18 +0200

    adding licence

--- tests/test_ssh.py
+++ tests/test_ssh.py
@@ -1,5 +1,22 @@
 #!/usr/bin/env python3
 
+# This file is part of pam-accesscontrol.
+#
+#    Copyright (C) 2019  Alexander Naumov <alexander_naumov@opensuse.org>
+#
+#    PAM-ACCESSCONTROL is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    PAM-ACCESSCONTROL is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with PAM-ACCESSCONTROL.  If not, see <http://www.gnu.org/licenses/>.
+
 from scp import SCPClient
 import os, sys
 import argparse
